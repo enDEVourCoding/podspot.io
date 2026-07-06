9a131bcccde4523b38271e1fe869ce1e9724dc9e
enDEVourCoding <endevour@atomicmail.io>
2026-07-06 02:18:30 +0000

    Test

---
+++ assets/css/js/player.js
@@ -0,0 +1,1 @@
+/* PODSPHERE v0.6 Galaxy Core · player.js placeholder module, intentionally tiny for vanilla extension. */
---
+++ assets/css/js/utils.js
@@ -0,0 +1,1 @@
+window.PS_VERSION={number:'0.6.0',codename:'Galaxy Core',build:'2026.07',engine:'Universe Engine'};window.$=(s,c=document)=>c.querySelector(s);window.$$=(s,c=document)=>[...c.querySelectorAll(s)];
